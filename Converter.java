e9b9a441901983d703fb3647a92a5b2847f846a4
unknown <abmbaziira@gmail.com>
2022-09-28 17:42:23 -0400

    Refactoring

--- Converter.java
+++ Converter.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Converter {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        DecimalFormat fmt = new DecimalFormat("0.###");
         System.out.println("Enter temperature in F:");
         double T = scan.nextDouble();
 
-        double K = T + 273.15;
+        double K = (T -32)*(5.0/9)+(273.15);
 
-        System.out.println("The temperature in kelvin is "+K);
+        System.out.println("The temperature in kelvin is "+fmt.format(K));
     }
 }
